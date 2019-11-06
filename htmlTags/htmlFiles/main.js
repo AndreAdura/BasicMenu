831b1c87ff867114941653523c65f9510d5dcbc6
Andre Henrique Adura <38469671+AndreAdura@users.noreply.github.com>
2019-11-06 09:09:22 -0800

    Add files via upload

---
+++ htmlTags/htmlFiles/main.js
@@ -0,0 +1,24 @@
+
+
+
+function Hello(){
+ let name = prompt("hello, what is your name?");
+ document.getElementById("demo").innerHTML = "Hello " + name;
+}
+
+function rain(){
+	let rain = prompt("Is it rainning?? 1 - Yes / 0 - No");
+if (rain == 1) {
+	alert("Take your umbrella");
+} else{
+	alert("Don't take your Umbrella");
+}
+}
+
+
+
+
+
+
+
+
